fbebfe145c85efec9607ce2149c3bdf044cd7ffa
Zhiyuan Yao <yaozyforwork@gmail.com>
2017-10-05 19:34:43 -0700

    add @PreAuthorized

--- src/main/java/com/zhiyuan/finance/banking_copy/controller/UserController.java
+++ src/main/java/com/zhiyuan/finance/banking_copy/controller/UserController.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -80,7 +82,7 @@ public class UserController {
 		return "Validated: " + userId;
 	}
 
-
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@RequestMapping(value="/name",method=RequestMethod.POST)
 	public User createUser(@RequestBody User userObj) {
 		
